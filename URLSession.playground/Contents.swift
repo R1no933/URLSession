b5e686cadd7698739a296acc989e970e9756d0dc
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-08-06 18:48:38 +0300

    complete_request
    -complete request
    -testing on Hong Kong Obervatory API

--- URLSession.playground/Contents.swift
+++ URLSession.playground/Contents.swift
@@ -1,14 +1,36 @@
 import UIKit
 
+let configureSession = URLSessionConfiguration.default
+configureSession.allowsCellularAccess = true
+configureSession.waitsForConnectivity = true
+let session = URLSession(configuration: configureSession)
+
 func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
     guard let url = urlRequest else { return }
-    URLSession.shared.dataTask(with: url) { data, response, error in
+    
+    session.dataTask(with: url) { (data, response, error) in
         if error != nil {
-            
+            print("Error is: \(error?.localizedDescription ?? "")")
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            print("Server responce is: \(response.statusCode)")
+            
             guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
+            print("Our data is: \(dataAsString ?? "Nothing")")
+            
+        } else if let response = response as? HTTPURLResponse {
+            print("Server responce is: \(response.statusCode)")
+            return
         }
+        
     }.resume()
 }
+
+let baseUrl = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?"
+let nineDayEngWeather = baseUrl + "dataType=fnd&lang=eng"
+let nineDayChWeather = baseUrl + "dataType=fnd&lang=tc"
+
+getData(urlRequest: baseUrl)
+getData(urlRequest: nineDayChWeather)
+getData(urlRequest: nineDayEngWeather)
